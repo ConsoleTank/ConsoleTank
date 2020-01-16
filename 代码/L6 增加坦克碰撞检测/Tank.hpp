4ae467ad977b2698a637e704d8bf352d23aea5b6
fyc <fyc@time-vr.com>
2020-01-16 12:23:07 +0800

    新增L6

---
+++ 代码/L6 增加坦克碰撞检测/Tank.hpp
@@ -0,0 +1,212 @@
+#pragma once
+#include "Common.h"
+#include "tools.hpp"
+#include"bullet.h"
+#include "GameMode.h"
+#include <list>
+#include "Map.hpp"
+class GameMode;
+
+class Tank {
+public:
+	~Tank()
+	{
+		for (auto i=my_bullet.begin();i!=my_bullet.end();i++)
+		{
+			delete (*i);
+			(*i) = NULL;
+		}
+
+		my_bullet.clear();
+
+		bul_num = 0;
+	}
+
+	
+	void draw_tank() {
+		static int tank_map[4][9] = { 
+		{0,5,0,5,5,5,5,0,5}, 
+		{5,0,5,5,5,5,0,5,0} ,
+		{0,5,5,5,5,0,0,5,5},
+		{5,5,0,0,5,5,5,5,0} 
+		};
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (GameMode::instance().m_pmap->map[(i + pos_x)*Common::LEN + pos_y + j] == Common::GRASS)
+					continue;
+				GameMode::instance().m_pmap->map[(i + pos_x)*Common::LEN + pos_y + j] = tank_map[(int)m_dir][i * 3 + j];
+			}
+		}
+
+		GameMode::instance().m_pmap->draw();
+	}
+
+	void clear()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (GameMode::instance().m_pmap->map[(i + pos_x)*Common::LEN + pos_y + j] == Common::GRASS)
+					continue;
+				GameMode::instance().m_pmap->map[(i + pos_x) * Common::LEN + pos_y + j] = Common::WALK;
+			}
+		}
+	}
+
+
+	void fire() {
+		bullet *bul = new bullet(this);
+		my_bullet.push_back( bul);
+		bul_num++;
+
+		bul->fire();
+	}
+	
+	void tank_tick()
+	{
+		for (auto i = my_bullet.begin(); i != my_bullet.end(); i++)
+		{
+			(*i)->tick();
+		}
+
+		for (auto i = my_bullet.begin(); i != my_bullet.end();)
+		{
+			if (!(*i)->m_bIsAlife)
+			{
+				bullet* temp = *i;
+				i = my_bullet.erase(i);
+				delete temp;
+			}
+			else
+				++i;
+		}
+	}
+
+	bool judge(int offset, bool if_col)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			int judge_op = 0;
+			if(if_col)
+				judge_op = GameMode::instance().m_pmap->map[(pos_x + offset)* Common::LEN + pos_y + i];
+			else
+				judge_op = GameMode::instance().m_pmap->map[(pos_x + i)* Common::LEN + pos_y + offset];
+
+			if (judge_op == Common::STONE || judge_op == Common::WALL || judge_op == Common::WATER)
+				return false;
+		}
+
+		return true;
+
+	}
+	
+	void Move(ETankDir dir)
+	{
+		clear();
+		
+		m_dir = dir;
+
+		switch (dir)
+		{
+		case E_DIR_T:
+		{
+			if (!judge(-1, true))
+				break;
+			else
+				pos_x--;
+			break;
+		}
+		case E_DIR_B:
+		{
+
+			if (!judge(3, true))
+				break;
+			else
+				pos_x++;
+			break;
+		}
+		case E_DIR_L:
+		{
+			if (!judge(-1, false))
+				break;
+			else
+				pos_y--;
+			break;
+		}
+		case E_DIR_R:
+		{
+
+			if (!judge(3, false))
+				break;
+			else
+				pos_y++;
+			break;
+		}
+		}
+
+		draw_tank();
+	}
+
+	void ProcessKeyBoard(char ch)
+	{
+		switch (ch)
+		{
+
+		case 'j':
+		case 'J':
+			fire();
+			break;
+
+		case 'q':
+		case 'Q':
+			Exit();
+			break;
+
+		case 'w':
+		case 'W':
+		{
+			Move(E_DIR_T);
+			break;
+		}
+		case's':
+		case'S':
+		{
+			Move(E_DIR_B);
+			break;
+		}
+		case'a':
+		case'A':
+		{
+			Move(E_DIR_L);
+			break;
+		}
+
+
+		case'd':
+		case'D':
+		{
+			Move(E_DIR_R);
+			break;
+		}
+		}
+	}
+
+	void Exit()
+	{
+		for (auto i = my_bullet.begin(); i != my_bullet.end(); i++)
+		{
+			(*i) ->clear();
+		}
+		GameMode::instance().ReturnToMainMenu();
+	}
+
+public:
+	int pos_x;
+	int pos_y;
+	list <bullet*>my_bullet;
+	int bul_num;
+	ETankDir m_dir;
+};
